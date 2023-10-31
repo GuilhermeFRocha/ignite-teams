796bc7bb71dc5f346afbfe49bf3a1489de224cf1
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-10-31 20:10:35 -0300

    create theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,5 @@
-import { Container, Title } from "./styles";
+import { Container } from "./styles";
 
 export function Groups() {
-  return (
-    <Container>
-      <Title>Grupos</Title>
-    </Container>
-  );
+  return <Container></Container>;
 }
