4c4812191b6a028e17180e4b7b20c3d838ac9e96
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-10-31 20:25:25 -0300

    fonts theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups";
 import theme from "./src/theme";
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
