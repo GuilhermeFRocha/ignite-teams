7ea52eb5ef5e6e3f313004244df20703c505ad7f
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-11-08 20:46:37 -0300

    create header

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator } from "react-native";
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups";
 import theme from "./src/theme";
@@ -17,6 +17,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
